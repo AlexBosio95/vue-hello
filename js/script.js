3e51dcd92d061b367d5526e691c3b427ff7977c9
AlexBosio95 <alex.bosio1995@gmail.com>
2022-07-04 18:36:35 +0200

    if fix

--- js/script.js
+++ js/script.js
@@ -3,12 +3,19 @@ const app = new Vue({
     data: {
         message: 'Photo Viewer',
         imgUrl: 'https://images.unsplash.com/photo-1627222055365-95a68d345df4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80',
-        newImgUrl: ''
+        newImgUrl: '',
+        numberOne: 0,
+        numberTwo: 0,
+        numberResult: 0
     },
 
     methods: {
         updateImg() {
-            this.imgUrl = this.newImgUrl;
+            if (this.newImgUrl == '') {
+                this.newImgUrl = this.imgUrl
+            } else {
+                this.imgUrl = this.newImgUrl;
+            }
         }
     }
 
